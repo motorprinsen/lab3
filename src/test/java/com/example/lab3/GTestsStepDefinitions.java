44cb9476ec38fd9b856f33acebd82c505c11dae0
Anders Ågren <anders.agren@rssoftware.se>
2023-04-06 11:06:16 +0200

    G skeleton

--- src/test/java/com/example/lab3/GTestsStepDefinitions.java
+++ src/test/java/com/example/lab3/GTestsStepDefinitions.java
@@ -1,92 +1,116 @@
 package com.example.lab3;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GTestsStepDefinitions {
 
-    static WebDriver driver;
+    private static WebDriver driver;
+    private static final int toWait = 5;
 
-    @Before
-    public void setupSelenium() {
-
-    }
-
-    @After
-    public void teardownSelenium() {
-
-    }
-
-    @Given("SVT Play is available")
-    public void svt_play_is_available() {
+    //@BeforeAll
+    public static void setup() {
         var options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*", "--incognito");
         driver = new ChromeDriver(options);
 
         driver.get("https://www.svtplay.se/");
-    }
-
-    @When("User visits SVT Play")
-    public void user_visits_svt_play() {
         driver.manage().window().maximize();
+
+        acceptCookieConsentDialog();
     }
 
-    @Then("The title should be {string}")
-    public void the_title_should_be(String expectedTitle) {
-        var actualTitle = driver.getTitle();
+    private static void acceptCookieConsentDialog() {
+        // The modal takes a couple of seconds to show up
+        var modalXpath = "//div[@data-rt='cookie-consent-modal']";
+        new WebDriverWait(driver, Duration.ofSeconds(toWait))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(modalXpath)));
 
-        assertEquals(expectedTitle, actualTitle, "Unexpected title");
+        // Accept the default settings
+        var modal = driver.findElement(By.xpath(modalXpath));
+        var consentButton = modal.findElement(By.xpath(".//button[text() = 'Acceptera alla']"));
+        consentButton.click();
+
+        // The modal takes a couple of seconds to close after accepting
+        new WebDriverWait(driver, Duration.ofSeconds(toWait))
+                .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(modalXpath)));
     }
 
-    @Then("The logo should be displayed")
+    //@AfterAll
+    public static void teardown() {
+        driver.quit();
+    }
+    @Given("SVT Play is available")
+    public void svt_play_is_available() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("a user visits SVT Play")
+    public void a_user_visits_svt_play() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the title should be {string}")
+    public void the_title_should_be(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the logo should be displayed")
     public void the_logo_should_be_displayed() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-
-    @Then("The main link texts should be {string}, {string} and {string}")
+    @Then("the main link texts should be {string}, {string} and {string}")
     public void the_main_link_texts_should_be_and(String string, String string2, String string3) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-
-    @Then("The link to the availability site {string} should be visible")
-    public void the_link_to_the_availability_site_should_be_visible(String string) {
+    @Then("the link to the availability page {string} should be visible")
+    public void the_link_to_the_availability_page_should_be_visible(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    @Then("The link text should be {string}")
+
+    @Then("the link text should be {string}")
     public void the_link_text_should_be(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-
-    @When("User navigates to the availability site")
-    public void user_navigates_to_the_availability_site() {
+    @When("navigates to the availability page {string}")
+    public void navigates_to_the_availability_page(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    @Then("The heading should be {string}")
+
+    @Then("the heading should be {string}")
     public void the_heading_should_be(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("User navigates to {string} page")
-    public void user_navigates_to_page(String string) {
+    @When("navigates to {string} page")
+    public void navigates_to_page(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    @Then("The number of categories should be {int}")
-    public void the_number_of_categories_should_be(Integer int1) {
+
+    @Then("the number of categories displayed should be {int}")
+    public void the_number_of_categories_displayed_should_be(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
