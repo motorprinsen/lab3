67390d1ca754ac414ffcd80f3574999ab08f9011
Anders Ågren <anders.agren@rssoftware.se>
2023-04-07 15:17:37 +0200

    WIP - VG tests

--- src/test/java/com/example/lab3/VGStepDefinitions.java
+++ src/test/java/com/example/lab3/VGStepDefinitions.java
@@ -5,16 +5,6 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 
 public class VGStepDefinitions {
-    @Given("a user navigates to start page")
-    public void a_user_navigates_to_start_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Given("accepts the cookie consent dialog")
-    public void accepts_the_cookie_consent_dialog() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
     @When("a user clicks on the newsletter link \\({string})")
     public void a_user_clicks_on_the_newsletter_link(String string) {
         // Write code here that turns the phrase above into concrete actions
@@ -30,92 +20,122 @@ public class VGStepDefinitions {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    @When("enter an email address")
-    public void enter_an_email_address() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("does not tick the consent box")
-    public void does_not_tick_the_consent_box() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
     @Then("the error message {string} should show")
     public void the_error_message_should_show(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    @When("a user clicks on {string}")
-    public void a_user_clicks_on(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the first listed program should have a start time before the current time")
-    public void the_first_listed_program_should_have_a_start_time_before_the_current_time() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("all programs should be listed correctly and according to the first letter in the program name")
-    public void all_programs_should_be_listed_correctly_and_according_to_the_first_letter_in_the_program_name() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("a user searches for {string}")
-    public void a_user_searches_for(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the first listed program should be {string}")
-    public void the_first_listed_program_should_be(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("no results should be shown")
-    public void no_results_should_be_shown() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the text \"Inget resultat för sökningen \"qwerty\"\" should be shown")
-    public void the_text_inget_resultat_för_sökningen_qwerty_should_be_shown() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("a user clicks on the first available program")
-    public void a_user_clicks_on_the_first_available_program() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("starts the video player")
-    public void starts_the_video_player() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("clicks the fullscreen button \\({string})")
-    public void clicks_the_fullscreen_button(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the player should be shown fullscreen")
-    public void the_player_should_be_shown_fullscreen() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("a user clicks on a program in the {string} section")
-    public void a_user_clicks_on_a_program_in_the_section(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the program should display an overlay indicating the time left to view it")
-    public void the_program_should_display_an_overlay_indicating_the_time_left_to_view_it() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+//    @Given("a user navigates to start page")
+//    public void a_user_navigates_to_start_page() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @Given("accepts the cookie consent dialog")
+//    public void accepts_the_cookie_consent_dialog() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("a user clicks on the newsletter link \\({string})")
+//    public void a_user_clicks_on_the_newsletter_link(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("does not enter an email address")
+//    public void does_not_enter_an_email_address() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("clicks the sign-up button \\({string})")
+//    public void clicks_the_sign_up_button(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("enter an email address")
+//    public void enter_an_email_address() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("does not tick the consent box")
+//    public void does_not_tick_the_consent_box() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @Then("the error message {string} should show")
+//    public void the_error_message_should_show(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("a user clicks on {string}")
+//    public void a_user_clicks_on(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("the first listed program should have a start time before the current time")
+//    public void the_first_listed_program_should_have_a_start_time_before_the_current_time() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @Then("all programs should be listed correctly and according to the first letter in the program name")
+//    public void all_programs_should_be_listed_correctly_and_according_to_the_first_letter_in_the_program_name() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("a user searches for {string}")
+//    public void a_user_searches_for(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("the first listed program should be {string}")
+//    public void the_first_listed_program_should_be(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @Then("no results should be shown")
+//    public void no_results_should_be_shown() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("the text \"Inget resultat för sökningen \"qwerty\"\" should be shown")
+//    public void the_text_inget_resultat_för_sökningen_qwerty_should_be_shown() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("a user clicks on the first available program")
+//    public void a_user_clicks_on_the_first_available_program() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("starts the video player")
+//    public void starts_the_video_player() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @When("clicks the fullscreen button \\({string})")
+//    public void clicks_the_fullscreen_button(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("the player should be shown fullscreen")
+//    public void the_player_should_be_shown_fullscreen() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("a user clicks on a program in the {string} section")
+//    public void a_user_clicks_on_a_program_in_the_section(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//
+//    @Then("the program should display an overlay indicating the time left to view it")
+//    public void the_program_should_display_an_overlay_indicating_the_time_left_to_view_it() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
 }
